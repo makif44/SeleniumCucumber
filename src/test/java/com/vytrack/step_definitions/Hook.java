0bbe7daa5d5adfd3853045b8b4faca20c4b650c9
cybertekschool <github@cybertekschool.com>
2020-02-14 11:51:40 -0500

    added mobile chrome

--- src/test/java/com/vytrack/step_definitions/Hook.java
+++ src/test/java/com/vytrack/step_definitions/Hook.java
@@ -13,22 +13,23 @@ public class Hook {
     private static Logger logger = Logger.getLogger(Hook.class);
 
     @Before
-    public void setup(){
+    public void setup() {
         logger.info("##############################");
         logger.info("Test setup!");
-        if(!ConfigurationReader.getProperty("browser").contains("remote")) {
-        Driver.get().manage().window().maximize();
+        String browser = ConfigurationReader.getProperty("browser");
+        if (!browser.contains("remote") && !browser.contains("mobile")) {
+            Driver.get().manage().window().maximize();
         }
     }
 
     @After
-    public void teardown(Scenario scenario){
+    public void teardown(Scenario scenario) {
         //if test failed - do this
-        if(scenario.isFailed()){
+        if (scenario.isFailed()) {
             logger.error("Test failed!");
-            byte[] screenshot = ((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
+            byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
             scenario.embed(screenshot, "image/png");
-        }else{
+        } else {
             logger.info("Cleanup!");
             logger.info("Test completed!");
         }
